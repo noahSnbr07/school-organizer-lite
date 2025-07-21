4c638481d1f29f4288938ac5f6979ee75563c62e
noahSnbr07 <nur.noah.saschenbrecker@gmail.com>
2025-07-22 00:33:58 +0200

    new file of constants: pills

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -1,9 +1,11 @@
 import favicon from "./images/favicon.svg";
 import waves from "./images/wave.svg";
 import banner from "./images/banner.svg";
+import hyperlinkPills from "./constants/hyperlink-pills-links";
 
 export {
     favicon,
     waves,
     banner,
+    hyperlinkPills,
 }
