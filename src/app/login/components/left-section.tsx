43f1ca0c41ec7163817af2bb6e7d12bd83816554
noahSnbr07 <nur.noah.saschenbrecker@gmail.com>
2025-07-24 21:35:13 +0200

    inserted new icon into login ui

--- src/app/login/components/left-section.tsx
+++ src/app/login/components/left-section.tsx
@@ -8,14 +8,16 @@ export default async function LeftSection() {
     return (
         <div className="flex bg-stack relative items-center justify-center">
             <Image
+                className="rounded-4xl shadow-2xl"
                 src={favicon}
                 alt="Icon"
                 title="Icon"
-                height={128}
-                width={128}
+                height={360}
+                width={360}
             />
-            <div className="absolute bottom-0 left-0 w-full h-24">
+            <div className="absolute bottom-0 left-0 w-full h-full">
                 <Image
+                    loading="eager"
                     src={waves}
                     alt="Waves"
                     title="Waves"
